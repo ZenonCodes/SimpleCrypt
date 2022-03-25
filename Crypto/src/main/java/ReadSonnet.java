f6943aad234ce59f3eb1f97dc52b300b142a37f7
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-03-25 13:07:23 -0400

    mvp finished

--- Crypto/src/main/java/ReadSonnet.java
+++ Crypto/src/main/java/ReadSonnet.java
@@ -13,6 +13,7 @@ public class ReadSonnet {
                 FileReader sonnetReader = new FileReader(sonnet);
                 BufferedReader reader = new BufferedReader(sonnetReader);
                 String line = null;
+                System.out.println("\n START ORIGINAL TEXT HERE:  \n");
                 while ((line = reader.readLine()) != null) {
                     sonneto.add(line);
                     System.out.println(line);
@@ -25,13 +26,33 @@ public class ReadSonnet {
 
         try{
             FileWriter sonnetWriter = new FileWriter("sonnet18.enc");
+            System.out.println("\n START ENCRYPTED TEXT HERE:  \n");
             for (String lyric: sonneto){
                 sonnetWriter.write(x.encrypt(lyric) + '\n');
+                System.out.println(x.encrypt(lyric));
             }
            sonnetWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try {
+            File sonnetDecrypt = new File("sonnet18.enc");
+            FileReader sonnetReader = new FileReader(sonnetDecrypt);
+            BufferedReader reader = new BufferedReader(sonnetReader);
+            String line = null;
+            System.out.println("\n START DECRYPTED TEXT HERE:  \n");
+            while ((line = reader.readLine()) != null) {
+                sonneto.add(line);
+                System.out.println(x.decrypt(line));
+            }
+            reader.close();
+        } catch (Exception e) {
+
+            e.printStackTrace();
         }
+        }
+
+
     }
 
